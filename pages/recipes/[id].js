db27b84648d2e56f1f37e82cff53e0cc8f30f1b0
Angelo Maiele <angelomaiele@gmail.com>
2022-10-25 21:32:29 -0400

    trying pass ata

--- pages/recipes/[id].js
+++ pages/recipes/[id].js
@@ -1,7 +1,59 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+  
+    // const options = {
+    //     method: "GET",
+    //     headers: {
+    //       "X-RapidAPI-Key": "cac0ff8c40msh8d6b13eceeb7d54p149d44jsn09a410dae8d1",
+    //       "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+    //     },
+    //   };
+    //   const res = await fetch(
+    //     "https://edamam-recipe-search.p.rapidapi.com/search?q=chicken",
+    //     options
+    //   );
+    //   const data = await res.json();
+    //   console.log(data);
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+    const data = await res.json();
+
+const paths = data.map(coment=> {
+return {
+        params: {id: coment.id.toString() }
+    }
+})
+
+return { 
+    paths,
+    fallback: false
+    
+}
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+//     const options = {
+//         method: "GET",
+//         headers: {
+//           "X-RapidAPI-Key": "cac0ff8c40msh8d6b13eceeb7d54p149d44jsn09a410dae8d1",
+//           "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+//         },
+//       };
+//   const res = await fetch("https://edamam-recipe-search.p.rapidapi.com/search=?q=chicken" + label ,options);
+//   const data = await res.json();
+const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments/' + id )
+const data = await res.json();
+
+  return {
+    props: { per: data },
+  };
+}
+
+const Details = ({per}) => {
     return (  
         <div>
-            <h1>Details Recipe</h1>
+            <h1 style={{color: "red"}}>{per.id}</h1>
+            <h1>{per.name}</h1>
         </div>
     );
 }
--- pages/recipes/recipes.js
+++ pages/recipes/recipes.js
@@ -2,42 +2,41 @@ import styles from "../../styles/Recipes.module.css";
 import Image from "next/image";
 import Link from "next/Link"
 export const getStaticProps = async () => {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "cac0ff8c40msh8d6b13eceeb7d54p149d44jsn09a410dae8d1",
-      "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
-    },
-  };
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "cac0ff8c40msh8d6b13eceeb7d54p149d44jsn09a410dae8d1",
+  //     "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+  //   },
+  // };
 
-  // const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
-  // const data = await res.json();
-
-  const res = await fetch(
-    "https://edamam-recipe-search.p.rapidapi.com/search?q=chicken",
-    options
-  );
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
   const data = await res.json();
 
-  console.log(data);
+  // const res = await fetch(
+  //   "https://edamam-recipe-search.p.rapidapi.com/search?q=chicken",
+  //   options
+  // );
+  // const data = await res.json();
 
-  // return {
-  //     props: {comments: data}
-  // }
 
   return {
-    props: { reci: data },
-  };
+      props: {comments: data}
+  }
+
+  // return {
+  //   props: { reci: data },
+  // };
 };
 
-const Recipes = ({ reci }) => {
+const Recipes = ({ comments }) => {
   return (
     <div>
       <h1 className={styles.titleRecipes}> recipes</h1>
-      {reci.hits.map((recipe) => (
-        <Link href={'/recipes/' + recipe.recipe.label} key={recipe.recipe.uri}>
+      {comments.map((comment) => (
+        <Link href={'/recipes/' + comment.id} key={comment.id}>
         <a className={styles.single}>
-        <h1>{recipe.recipe.label}</h1>
+        <h1>{comment.email}</h1>
         </a>
         </Link>
       ))}
